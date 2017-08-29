909483147e28098142989c538aa869521b6f287c
karthikpamidimarri <karthikpamidimari@gmail.com>
2017-08-29 16:24:12 +0530

    Step 10: Set a static default props to a component for component existence (The purpose of default props is to ensure that when a component begins its existence, it has certain props available to it right away. )

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
 
 class App extends Component {
+
   render() {
+    console.log(this.props)
     return (
         <h1>hello</h1>
     )
   }
 }
 
-App.displayName = "Whatever-we-wwant"
+App.displayName = "WhatEverWeWant"
+
+App.defaultProps = {
+  someImportArray:[]
+}
 export default App
