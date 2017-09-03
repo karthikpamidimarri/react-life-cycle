d7187142c998ebecc8df6fa1a68c6af62b59ea82
karthikpamidimarri <karthikpamidimari@gmail.com>
2017-09-03 12:26:09 +0530

    Step 15: Setting the state in componentDidMount

--- src/App.js
+++ src/App.js
@@ -2,6 +2,19 @@ import React, { Component } from 'react'
 import loggify from './loggify'
 class App extends Component {
 
+  fetchData = () =>{
+    console.log("Going to fetch Data")
+    setTimeout(() => {
+      console.log("Data retrieved")
+      this.setState({
+          data: Math.random
+      })
+    },1500)
+  }
+
+  componentDidMount(){
+    this.fetchData()
+  }
   render() {
     return (
       <div>
