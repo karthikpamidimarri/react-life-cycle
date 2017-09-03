3e8118a65d5aa5effcdc8512e33ff4819fcf818e
karthikpamidimarri <karthikpamidimari@gmail.com>
2017-09-03 13:14:05 +0530

    Step 15.3: Accessing stateless components in componentDidMount function using Refs

--- src/App.js
+++ src/App.js
@@ -1,33 +1,54 @@
 import React, { Component } from 'react'
 import loggify from './loggify'
+
 class App extends Component {
-  state = {
-      data: "No data yet!"
-  }
 
-  fetchData = () =>{
-    console.log("Going to fetch Data")
-    setTimeout(() => {
-      console.log("Data retrieved")
-      this.setState({
-          data: Math.random()
-      })
-    },1500)
-  }
+    static displayName = "App"
 
-  componentDidMount(){
-    this.fetchData()
-  }
-  render() {
-    return (
-      <div>
-        hello
-        <h2>{this.state.data}</h2>
-      </div>
-    )
-  }
+    state = {
+        data: "No Data yet!"
+    }
+
+    fetchData = () => {
+        console.log("Going to fetch data!")
+        setTimeout(
+            () => {
+                console.log("Data retrieved")
+                this.setState({
+                    data: Math.random()
+                })
+            },
+            1500
+        )
+    }
+
+    componentDidMount(){
+        this.fetchData()
+        const canvasCtx = this.refs.appCanvas.getContext('2d')
+        canvasCtx.fillStyle = "blue"
+        canvasCtx.arc(75, 75, 50, 0, 2 * Math.PI )
+        canvasCtx.fill()
+    }
+
+
+    render() {
+        return (
+            <div>
+              Hello
+
+              <h4>{this.state.data}</h4>
+
+              <canvas
+                  ref={"appCanvas"}
+                  height={200}
+                  width={200}
+              />
+
+            </div>
+        )
+    }
 }
-App.displayName = "App"
+
 App = loggify(App)
 
 export default App
